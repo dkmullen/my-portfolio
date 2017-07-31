f12f88be4cd38a491f7e5b3adaf726dc852e0917
Dennis Mullen <dkmullen@gmail.com>
2017-07-31 19:52:33 -0400

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 /*jshint esversion: 6 */
 
 const gulp = require('gulp'),
-  watch = require('gulp-watch'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  cssvars = require('postcss-simple-vars'),
-  nested = require('postcss-nested');
+  watch = require('gulp-watch'), // monitor changes to files and react
+  postcss = require('gulp-postcss'), // main package to add post-processing to css
+  autoprefixer = require('autoprefixer'), // adds vendor prefixes when needed
+  cssvars = require('postcss-simple-vars'), // processes vars into valid css
+  nested = require('postcss-nested'), // processes nested css into valid css
+  cssImport = require('postcss-import'), // Allows for importing css as modules
+  mixins = require('postcss-mixins'), // mixins are reusable bits of code
+  hexrgba = require('postcss-hexrgba'); // allows use of hex variables w/in rgba brackets
 
 gulp.task('default', () => {
   console.log("Here is my default task.");
@@ -16,9 +19,14 @@ gulp.task('html', () => {
 });
 
 gulp.task('styles', () => {
-  return gulp.src('./app/resources/styles/styles.css')
-    .pipe(postcss([nested, cssvars, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles.css'));
+  return gulp.src('./app/resources/styles/styles.css') // Get this file...
+    // ...run it thru these...
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .on('error', function(errorInfo) { // this crashes w/ fat-arrow function
+      console.log(errorInfo.toString());
+      this.emit('end'); // End this task gracefully on error
+    })
+    .pipe(gulp.dest('./app/temp/styles')); // ...then put it here.
 });
 
 gulp.task('watch', () => {
